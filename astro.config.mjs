d32a6ddb757c39417ff8aa82bb138c12080bfc56
Ellie Foote <eleanorjfoote@gmail.com>
2023-09-25 21:12:10 +0200

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   output: "hybrid",
 });
