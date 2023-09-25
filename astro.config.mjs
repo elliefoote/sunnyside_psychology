b9e36bcba0c382fd8f816ec6aecc10b9ea53dac9
Ellie Foote <eleanorjfoote@gmail.com>
2023-09-25 20:50:50 +0200

    rm image tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import { astroImageTools } from "astro-imagetools";
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools],
+  integrations: [tailwind()],
   adapter: node({
     mode: "standalone",
   }),
