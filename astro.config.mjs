54f23efa098310d9fc792b9a02d2370c27daf407
Ellie Foote <eleanorjfoote@gmail.com>
2023-09-25 21:15:58 +0200

    back to serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
